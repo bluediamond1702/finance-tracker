663248cc471d4b62491b50e58f05d0796d8922a8
Greed1702 <bluediamond1702@gmail.com>
2017-02-19 00:20:35 +0700

    Add friendship and friends search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users, :controller => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friend', to: 'users#search'
+  post 'add_friend', to: 'user#add_friend'
 end
